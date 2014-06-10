e5c1996358250d9b75f6e68b8a914601d56a38f3
LukaszCaputa <lukasz@lukaszcaputa.eu>
2014-06-10 21:06:59 +0200

    Finally fixed AJAX

--- js/controllers.js
+++ js/controllers.js
@@ -4,7 +4,11 @@
 
 angular.module('myApp.controllers', [])
   .controller('MyCtrl1', ['$scope','$http', function($scope, $http) {
-  	$scope.data = {place: 'erdberg', sport: 'squash', result:'a'};
+
+
+
+
+  	$scope.data = {place: 'erdberg', sport: 'squash', result:''};
     $http.defaults.useXDomain = true;
 
   	$scope.reset = function() {
@@ -12,50 +16,34 @@ angular.module('myApp.controllers', [])
     };
 
     $scope.send = function() {
- /*   	var url = 'http://www.clubdanube.at/appdata/index.php?e=ballsport&s=S220120604&n=Squash&d=2014-06-08&ft=19%3A00%3A00&tt=21%3A00%3A00';
-    	var method = 'GET';
-    	var xhr = new XMLHttpRequest();
-  if ("withCredentials" in xhr) {
-    // XHR for Chrome/Firefox/Opera/Safari.
-    xhr.open(method, url, true);
-    
-  } else if (typeof XDomainRequest != "undefined") {
-    // XDomainRequest for IE.
-    xhr = new XDomainRequest();
-    xhr.open(method, url);
-  } else {
-    // CORS not supported.
-    xhr = null;
-    alert('CORS not supported');
-    
-  }
+      var mainUrl = 'http://lukaszkoweapi.herokuapp.com/nocache/?url=http://www.clubdanube.at/appdata/index.php?e=ballsport';
+      var placeUrl;
+      if($scope.data.place == 'erdberg'){
+        placeUrl = 'S220120604';
+      } else if ($scope.data.place == 'ottakring'){
+        placeUrl = 'S42012060';
+      }else{
+        alert($scope.data.place)
+      }
 
-  xhr.onload = function() {
-    var text = xhr.responseText;
-    alert('Response from CORS request to ' + url + ': ' + text);
-  };
-
-  xhr.onerror = function() {
-    alert('Woops, there was an error making the request.');
-  };
-
-  xhr.send();*/
-
-  //delete $http.defaults.headers.common['X-Requested-With'];
-  //delete $httpProvider.defaults.headers.common['X-Requested-With'];
-  $http.defaults.useXDomain = true;
-//$http.get('http://www.corsproxy.com/www.clubdanube.at/appdata/index.php?e=ballsport&s=S220120604&n=Squash&d=2014-06-10&ft=19%3A00%3A00&tt=21%3A00%3A00').success(function(data, status, headers, config){
-$http.get('http://www.clubdanube.at/appdata/index.php?e=ballsport&s=S220120604&n=Squash&d=2014-06-10&ft=19%3A00%3A00&tt=21%3A00%3A00').success(function(data, status, headers, config){
+      var sportUrl;
+      if($scope.data.sport == 'squash'){
+        sportUrl = 'Squash';
+      } else if ($scope.data.sport == 'badminton'){
+        sportUrl = 'Badminton';
+      } else if ($scope.data.sport == 'tennis'){
+        sportUrl = 'Tennis';
+      }else{
+        alert($scope.data.place)
+      }
+      $http.defaults.useXDomain = true;
+      $http.get(mainUrl+'%26s='+placeUrl+'%26n='+sportUrl+'%26d=2014-06-11%26ft=19%3A00%3A00%26tt=21%3A00%3A00').success(function(data, status, headers, config){
         // With the data succesfully returned, call our callback
         $scope.data.result = data;
-    }).error(function(data, status, headers, config){
+      }).error(function(data, status, headers, config){
         alert("error");
         $scope.data.result = config;
-    });
-
-
-
-
+      });
       
     };
 
